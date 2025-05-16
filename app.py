2352fbf8c7c643192824484605b985723666f475
ayshajaved <ayshajavd@gmail.com>
2025-05-16 14:40:20 +0500

    commit

--- app.py
+++ app.py
@@ -132,4 +132,4 @@ def health_check():
     return jsonify({'status': 'healthy'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
